d297ca697fd9e09a0bf279e5d8315f556a69a5de
Alex Drask Ivanov <inceptiongodmaxwell@gmail.com>
2021-01-30 18:32:01 +0100

    Mejoras estéticas
    ·Corrección de errores:
       *N/A
    
    ·Cambios y mejoras:
       *Se ha mejorado la pantalla de inicio de sesion. Ahora es más
        vistosa e intuitiva.

--- index.php
+++ index.php
@@ -9,18 +9,39 @@ Página de acceso para usuarios registrados.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
-        <link href="Estilo_basico.css" rel="stylesheet" type="text/css"/>
+        <link rel="stylesheet" href="Login.css">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;1,100&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
     <body>
-        <h1>Iniciar sesion</h1>
-        
-        <form action="Menu.php" method="post">
-            Usuario: <input type="text" name="usuario">
-            <br>
-            Contraseña: <input type="password" name="clave">
-            <br><br>
-            <input type="submit" name="enviar" value="Acceder">
-        </form>
-    </body>
+    <div class="contenedor">
+        <div class="caja-login">
+            <div class="logo">
+                <img src="Imagenes/duck.png" alt="" id="imgLogo">
+            </div>
+            <div class="titulo">
+                <h1>INICIAR SESION</h1>
+            </div>
+            <form action="Menu.php" method="post" name="formulario">
+            <div class="textbox">
+                <i class="fa fa-user"></i>
+                <input type="text" placeholder="Usuario" name="usuario">
+            </div>
+            <div class="textbox">
+                <i class="fa fa-lock"></i>
+                <input type="password" placeholder="Clave" name="clave">
+            </div>
+            <div class="checkbox">
+                <input type="checkbox" id="recuerdame">
+                <label for="recuerdame">Recordar clave</label>
+                <p><a href="#">Clave olvidada</a></p>
+            </div>
+            <input type="submit" class="boton" value="Acceder">
+            </form>
+        </div>
+    </div>
+
+</body>
 </html>
